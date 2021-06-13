daa6108c384dbad2090b3e151f37c7db1bd05718
kapilsoni2013 <kapilsoni2013@gmail.com>
2021-06-14 01:05:58 +0530

    config server setup

--- bookservice/src/main/java/com/kapilsony/bookservice/BookserviceApplication.java
+++ bookservice/src/main/java/com/kapilsony/bookservice/BookserviceApplication.java
@@ -1,5 +1,7 @@
 package com.kapilsony.bookservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,10 +10,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy(proxyTargetClass=true)
-public class BookserviceApplication {
+public class BookserviceApplication implements CommandLineRunner {
+
+	@Value("${testprop}")
+	private String profile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookserviceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("=====>>>"+profile);
+	}
 }
