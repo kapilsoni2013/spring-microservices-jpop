c140344bcf22c74fc5ac7c5768e7fa218783d5c4
kapilsoni2013 <kapilsoni2013@gmail.com>
2021-06-14 13:50:32 +0530

    shift common prop in config-store

--- user-service/src/main/java/com/kapilsony/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kapilsony/userservice/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.kapilsony.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
