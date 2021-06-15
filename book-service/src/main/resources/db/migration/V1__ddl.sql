ee52bb9f9a731bb6d60708842b1ecb4693581804
kapilsoni2013 <kapilsoni2013@gmail.com>
2021-06-15 17:31:44 +0530

    adding flyway in book-service

--- book-service/src/main/resources/db/migration/V1__ddl.sql
+++ book-service/src/main/resources/db/migration/V1__ddl.sql
@@ -1,13 +1,13 @@
-
-CREATE TABLE `books` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `author` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `create_time` datetime(6) NOT NULL,
-  `update_time` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_eh6bfj824qyn40pii29i90bll` (`name`)
+CREATE SCHEMA IF NOT EXISTS jpopdb;
+CREATE TABLE IF NOT EXISTS jpopdb.books (
+  id bigint NOT NULL AUTO_INCREMENT,
+  author varchar(255) DEFAULT NULL,
+  category varchar(255) DEFAULT NULL,
+  description varchar(255) DEFAULT NULL,
+  name varchar(255) DEFAULT NULL,
+  price float DEFAULT NULL,
+  create_time datetime(6) NOT NULL,
+  update_time datetime(6) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_eh6bfj824qyn40pii29i90bll (name)
 );
